79c1b999fc2f180aca8d87abb1be703eba1ce477
Marcus <swemad19@htl-kaindorf.at>
2023-04-21 13:01:32 +0200

    TS-IGNORE

--- src/main/webapp/schachRequests.ts
+++ src/main/webapp/schachRequests.ts
@@ -2,6 +2,7 @@ let NAME = "";
 let GAMEID = 0;
 
 const firstLoad = () => {
+    // @ts-ignore
     loadField();
     document.getElementById("popUp").innerHTML = "<div style=\"background-color: red; position: absolute;  top: 56%; left: 50%;transform: translate(-50%, -50%); height: 50vh; width: 50vh;\"><h1 style=\"text-align: center;\">ALERT!</h1><p style=\"text-align: center; font-size: larger;\">please input your name:</p><div style=\"display: flex; justify-content: center; align-items: center;\"><input type=\"text\" id=\"name\"/><button onclick=\"nameInputed();\">go!</button></div></div>"
     document.getElementById("popUp").style.backgroundColor = "red";
@@ -18,6 +19,7 @@ const startGame = () => {
     document.getElementById("startGameButton").style.display = "none";
     document.getElementById("joinGameButton").style.display = "none";
 
+    // @ts-ignore
     const url: string = `http://localhost:8080/pmz-1.0-SNAPSHOT/api/chess/start`;
     fetch(url)
         .then(res => {
@@ -37,6 +39,7 @@ const startGame = () => {
 
     document.getElementById("gameID").innerHTML ="your gameID is: "+GAMEID;
 
+    // @ts-ignore
     const url: string = `http://localhost:8080/pmz-1.0-SNAPSHOT/api/chess/board/` +GAMEID;
     fetch(url)
         .then(res => {
@@ -64,6 +67,7 @@ const joinGame = () => {
 
 }
 const hashInputed = (): void => {
+    // @ts-ignore
     GAMEID = (document.getElementById("hash") as HTMLInputElement).value;
     document.getElementById("popUp").style.display = "none";
     console.log("your hash is: " + GAMEID);
